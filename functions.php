deaad247bf444e2fb15b03bd1b7cf7aa5fd4da3d
Ethan Turner <ezturner@washington.edu>
2017-01-11 10:30:16 -0800

    Added a function for filtering out templates

--- functions.php
+++ functions.php
@@ -11,4 +11,13 @@ function my_theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+// Remove any templates from the UW Marketing theme that will not be used
+
+function tfc_remove_page_templates( $templates ) {
+    unset( $templates['templates/template-no-title.php'] );
+    return $templates;
+}
+
+add_filter( 'theme_page_templates', 'tfc_remove_page_templates' );
 ?>
