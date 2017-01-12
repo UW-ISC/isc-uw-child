b86564ea0666cb38e54ba05a78407e02435630d0
abhi3 <abhi3@uw.edu>
2017-01-12 11:55:50 -0800

    Makes template-article-section.php compatible with the new theme

---
+++ templates/template-article-section.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: Article Section
+ *
+ * A full-width template, that displays the description of a specific article
+ * as well as listing the children pages within it
+ *
+ * @author Abhishek Chauhan <abhi3@uw.edu>
+ */
+
+get_header();
+?>
+
+<section class="uw-body container">
+    <div class="row">
+
+        <?php get_template_part( 'menu', 'mobile' ); ?>
+        <?php get_template_part( 'breadcrumbs' ); ?>
+
+        <article class="uw-body-copy col-lg-12">
+
+            <?php
+            while ( have_posts() ) : the_post();
+                the_title( '<h3 class="title">', '</h3>' );
+                the_content();
+                edit_post_link();
+            endwhile;
+
+            ?>
+
+        </article>
+
+    </div>
+</section>
+
+<?php get_footer();
