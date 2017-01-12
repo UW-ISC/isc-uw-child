769e491405f5987b2ddb8c8328d62ca1b9251795
abhi3 <abhi3@uw.edu>
2017-01-12 11:28:06 -0800

    Makes article-page.php compatible with the new theme

--- functions.php
+++ functions.php
@@ -20,4 +20,72 @@ function tfc_remove_page_templates( $templates ) {
 }
 
 add_filter( 'theme_page_templates', 'tfc_remove_page_templates' );
+
+/**
+ * Displays the child pages of the current page as well as presenting a
+ * table of contents that links to each individual child page's content
+ *
+ * @author Kevin Zhao <zhaok24@uw.edu>
+ * @author Abhishek Chauhan <abhi3@uw.edu>
+ */
+
+if ( ! function_exists( 'display_child_pages_with_toc' ) ) :
+    function display_child_pages_with_toc() {
+      // The following lines grab all the children pages of the current page
+      $args = array(
+        'parent' => get_the_ID(),
+        'hierarchical' => 0,
+        'sort_column' => 'menu_order',
+        'sort_order' => 'asc'
+      );
+      $children_pages = get_pages($args);
+      $toc = count($children_pages) > 0;
+      if ($toc) {
+        // Fancy title of Table of Contents
+        $html = '<div class="isc-toc" id="toc"><p class="h4 beefy gold">Table of Contents</p><span class="slant xs short gold"></span>';
+
+        // Echoing each children page's title
+        foreach ($children_pages as $child) {
+            $html .= '<div><a href="#'.$child->post_name.'">';
+            $html .= $child->post_title;
+            $html .= '</a></div>';
+        }
+        // Ending the Table of Contents
+        $html .= '</div>';
+      }
+      foreach ($children_pages as $child) {
+          // Displaying the title of a child page
+          $url = get_permalink($child);
+          $html .= '<h4 class="title" id="'.$child->post_name.'"> <a href="'.$url.'">';
+          $html .= $child->post_title;
+          $html .= '</a> </h4>';
+          // Displaying the date the page was last updated
+          $date_updated = new DateTime($child->post_modified_gmt);
+          $html .= '<div id="date_updated"> Updated: ';
+          $html .= date_format($date_updated, 'm.d.y');
+          $html .= '</div>';
+          // Displaying the tags of a child page
+          $posttags = get_the_tags($child->ID);
+          if ($posttags) {
+            $html .= '<div id="tags"> Tags: ';
+            for ($x = 0; $x < count($posttags) - 1; $x++) {
+              $tag =  $posttags[$x];
+              $link = get_tag_link($tag);
+              $html .= '<a id="tag" href='.$link.'>'.$tag->name.', </a>';
+            }
+            $finaltag = $posttags[count($posttags) - 1];
+            $html .= '<a id="tag" href='.get_tag_link($finaltag).'>'.$finaltag->name.' </a>';
+            $html .= '<br>';
+          }
+          // Displaying the content
+          $html .= $child->post_content;
+          $html .= '<br>';
+          if ($toc) {
+            $html .= '<p class="isc-toc-top-btn"><a href="#toc">Return to top</a></p>';
+          }
+      }
+      echo $html;
+    }
+endif;
+
 ?>
---
+++ templates/template-article-page.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Article Page
+ *
+ * A full-width template, that displays the description of a specific article
+ * as well as listing the children pages within it
+ *
+ * @author Abhishek Chauhan <abhi3@uw.edu>
+ */
+
+get_header();
+get_template_part( 'header', 'image' );
+?>
+
+<section class="uw-body container">
+    <div class="row">
+
+        <?php get_template_part( 'menu', 'mobile' ); ?>
+        <?php get_template_part( 'breadcrumbs' ); ?>
+
+        <article id='main_content' class="uw-body-copy col-lg-12" tabindex="-1">
+
+            <?php
+            while ( have_posts() ) : the_post();
+                the_title( '<h3 class="title">', '</h3>' );
+                the_content();
+            endwhile;
+
+            display_child_pages_with_toc();
+            ?>
+
+        </article>
+
+    </div>
+</section>
+
+<?php get_footer();
