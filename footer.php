076a2878bfbb04e5844f143e0cca0f3f9a1dad61
Charlon Palacay <charlon@gmail.com>
2017-01-17 11:56:08 -0800

    Footer placeholder html and css.

--- footer.php
+++ footer.php
@@ -1,11 +1,27 @@
 <div role="contentinfo" class="uw-footer">
 
     <a href="http://www.washington.edu" class="footer-wordmark">University of Washington</a>
-
     <a href="http://www.washington.edu/boundless/"><h3 class="be-boundless">Be boundless</h3></a>
 
-    <h4>asdfasdf with us:</h4>
 
+    <div class="container" style="text-align:left; color:#fff; margin-bottom:30px;">
+        <div class="row">
+
+            <div class="col-md-4">
+                <h5>Contact Us</h5>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus turpis vel tincidunt posuere. Morbi in purus eleifend, imperdiet velit vitae, congue metus. Donec suscipit justo orci, ut accumsan massa sagittis ut. Morbi fringilla aliquam nulla in lacinia.
+            </div>
+            <div class="col-md-4">
+                <h5>HR Resources</h5>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus turpis vel tincidunt posuere. Morbi in purus eleifend, imperdiet velit vitae, congue metus. Donec suscipit justo orci, ut accumsan massa sagittis ut. Morbi fringilla aliquam nulla in lacinia.
+            </div>
+            <div class="col-md-4">
+                <h5>UW Resources</h5>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus turpis vel tincidunt posuere. Morbi in purus eleifend, imperdiet velit vitae, congue metus. Donec suscipit justo orci, ut accumsan massa sagittis ut. Morbi fringilla aliquam nulla in lacinia.
+            </div>
+
+        </div>
+    </div>
 
     <p>&copy; <?php echo date("Y"); ?> University of Washington  |  Seattle, WA</p>
 
