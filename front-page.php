f6cb4825e2958c6d76fc5dd422b3d97feb036a75
Charlon Palacay <charlon@gmail.com>
2017-01-19 14:54:10 -0800

    Hero banner rework per stakeholders.

--- front-page.php
+++ front-page.php
@@ -27,45 +27,34 @@
       <?php uw_site_title(); ?>
       <?php get_template_part( 'menu', 'mobile' ); ?>
 
-      <div class="" style="background: #0f0403 url(<?php echo $url ?>); height:500px; background-size:65%; background-position:110%; background-repeat:no-repeat;">
+      <div class="" style="background: #0f0403 url(<?php echo $url ?>); min-height:500px; background-size:65%; background-position:110%; background-repeat:no-repeat;">
           <div class="container">
 
             <div class="row">
                 <div class="col-md-6">
-
-                    <h2 class="sr-only">Search</h2>
-                    <div style="font-size:50px; color:#fff; font-weight: 900; font-family:'Encode Sans Compressed', sans-serif; text-transform:uppercase; line-height: 50px; margin: 50px 0;">One Place.<br>All your HR &amp; Payroll Questions</div>
-
-                    <form role="search" method="get" id="searchform" class="searchform" action="<?php echo get_site_url() ?>">
-                    	<div>
-                    		<label class="screen-reader-text" for="s">Search for:</label>
-                    		<input type="text" value="" name="s" id="s" placeholder="Search for:" autocomplete="off">
-                    		<input type="submit" id="searchsubmit" value="Search">
-                    	</div>
-                    </form>
+                    <h2 class="sr-only">Workday</h2>
+                    <div style="font-size:50px; color:#fff; font-weight: 900; font-family:'Encode Sans Compressed', sans-serif; text-transform:uppercase; line-height: 50px; margin: 50px 0 20px 0;">Introducing Workday</div>
+                    <span class="udub-slant"><span></span></span>
+                    <p style="color:#fff;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec augue rutrum tortor ornare blandit. Duis est ante, tempor a tortor nec, ornare consequat lectus. </p>
+                    <a class="uw-btn" href="#">Sign in to Workday</a>
                 </div>
-            </div>
-            <div class="row">
-
-                <div class="col-md-8" style="margin-top:20px;">
+                <div class="col-md-4 col-md-offset-2" style="margin-top:200px;">
                     <h2 class="sr-only">Quicklinks</h2>
                     <p>popular topics</p>
                     <a class="btn-sm uw-btn" href="#">Ask for help!</a>
                     <a class="btn-sm uw-btn" href="#">Learn about Timesheets</a>
                     <a class="btn-sm uw-btn" href="#">New Hires: Stare here!</a>
                 </div>
-                <div class="col-md-4">
-                    <div style="background: #fff; padding: 20px; margin-top:-30px; -webkit-box-shadow: 0 0 4px rgba(164,164,164,.5); box-shadow: 0 0 4px rgba(164,164,164,.5); ">
-                        <p>Introducing Workday</p>
-                        <a class="uw-btn" href="#">Sign in to Workday</a>
-                    </div>
-                </div>
+            </div>
+            <div class="row">
+
+
             </div>
 
           </div>
       </div>
 
-      <div id='main_content' class="container uw-body-copy" tabindex="-1" style="margin-top: -90px;">
+      <div id='main_content' class="container uw-body-copy" tabindex="-1" style="margin-top: -100px;">
 
           <div class="row">
 
