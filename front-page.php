49beb1e4114c6f9dfe18479752d6b35faa5fde7d
Charlon Palacay <charlon@gmail.com>
2017-01-19 09:05:07 -0800

    Fixes featured page learn more link.

--- front-page.php
+++ front-page.php
@@ -105,7 +105,7 @@
                               ?>
 
                               <p> <?php echo $summary; ?> </p>
-                              <p><a class="uw-btn btn-sm" href="<?php echo get_page_link($page->ID); ?>">learn more</a></p>
+                              <p><a class="uw-btn btn-sm" href="<?php echo get_permalink($featured_page->ID); ?>">learn more</a></p>
 
                             </div>
                         </div>
