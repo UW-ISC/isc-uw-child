b55d4f5235cc54a517ec6bb8bac3f6169be04e13
Craig M. Stimmel <cstimmel@uw.edu>
2017-01-24 15:26:52 -0800

    Remove unneeded enque call.

--- functions.php
+++ functions.php
@@ -1,19 +1,5 @@
 <?php
 
-function my_theme_enqueue_styles() {
-
-    $parent_style = 'parent-style';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
-}
-
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-
 /**
  * Set up the child / parent relationship and customize the UW object.
  */
@@ -26,7 +12,9 @@ if (!function_exists('setup_uw_object')){
     }
 }
 
-// Remove any templates from the UW Marketing theme that will not be used
+/**
+ * Remove any templates from the UW Marketing theme that will not be used
+ **/
 function tfc_remove_page_templates( $templates ) {
     unset( $templates['templates/template-no-title.php'] );
     return $templates;
