281fdadeed775e123b5b115dfb20a824b6e148e0
Charlon Palacay <charlon@gmail.com>
2017-02-09 11:39:56 -0800

    Fixes article section template.

--- templates/template-article-section.php
+++ templates/template-article-section.php
@@ -23,7 +23,7 @@ get_header();
 
     <div class="row">
 
-        <article class="uw-body-copy col-md-offset-1 col-md-10">
+        <article class="uw-body-copy col-md-8">
 
             xxxx this is an article section template xxxx
 
