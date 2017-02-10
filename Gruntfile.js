4b0c983701a5026e663a57ade8cf4f9958b9a696
Craig M. Stimmel <cstimmel@uw.edu>
2017-02-09 17:38:34 -0800

    Run jshint on javascript files when they are saved.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: ['Gruntfile.js', 'assets/js/*.js', 'package.json'],
+        },
 		sass: {
 			dist: {
 				files: {
@@ -12,10 +15,15 @@ module.exports = function(grunt) {
 			css: {
 				files: '**/*.scss',
 				tasks: ['sass']
-			}
+			},
+            javascript: {
+                files: ['<%= jshint.files %>'],
+                tasks: ['jshint']
+            }
 		}
 	});
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default',['watch']);
-}
+};
