9703951b9d468574ba9c50b7c5531345f086531b
Charlon Palacay <charlon@gmail.com>
2017-02-16 11:16:24 -0800

    Adds search override template.

---
+++ search.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+
+<?php uw_site_title(); ?>
+<?php get_template_part('menu', 'mobile'); ?>
+
+<div class="container uw-body">
+
+    <div class="row">
+        <div class="col-md-12">
+            <?php get_template_part('breadcrumbs'); ?>
+        </div>
+    </div>
+
+  <div class="row">
+
+    <div class="uw-content col-md-9" role='main'>
+
+        <div id='main_content' class="uw-body-copy" tabindex="-1">
+
+            xxxx this template uses search.php xxxx
+
+            <h2>Search Results</h2>
+
+            <div>
+
+                <?php
+                  if ( have_posts() ) :
+                    while ( have_posts() ) : the_post();
+                      get_template_part( 'content', 'archive' );
+                    endwhile;
+                  else :
+                    echo '<h3 class=\'no-results\'>Sorry, no results matched your criteria.</h3>';
+                  endif;
+                ?>
+
+                <div><?php posts_nav_link(' '); ?></div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
