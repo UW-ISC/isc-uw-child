c9629bfa2e0b8d3c25b96e0cb9525df86c113e20
Craig M. Stimmel <cstimmel@uw.edu>
2017-02-24 14:31:12 -0800

    Fixed phpcs errors and beautify in FooterLinks.

--- setup/class.isc-footer-links.php
+++ setup/class.isc-footer-links.php
@@ -1,26 +1,34 @@
 <?php
-
 /**
- * ISC FooterLinks
+ * ISC_FooterLinks
+ *
  * This installs the footer menu location.
+ *
+ * @package isc-uw-child
  */
 
-// TODO: remove unused functions!
-class ISC_FooterLinks
-{
+/**
+ * ISC_FooterLinks
+ */
+class ISC_FooterLinks {
 
-    const NAME              = 'Footer Links';
-    const LOCATION       = 'footer-links';
 
-    function __construct()
-    {
-        $this->menu_items = array();
-        add_action('after_setup_theme', array( $this, 'register_footer_links'));
-    }
+	const NAME              = 'Footer Links';
+	const LOCATION       = 'footer-links';
 
-    function register_footer_links()
-    {
-        register_nav_menu(self::LOCATION, __(self::NAME));
-    }
+	/**
+	 * Constructor method
+	 */
+	function __construct() {
+		$this->menu_items = array();
+		add_action( 'after_setup_theme', array( $this, 'register_footer_links' ) );
+	}
+
+	/**
+	 * Registration method
+	 */
+	function register_footer_links() {
+		register_nav_menu( self::LOCATION, __( self::NAME ) );
+	}
 
 }
