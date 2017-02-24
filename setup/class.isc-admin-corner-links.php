db2191d0b6902ab4df61d2d8fc8e0aa258d41705
Craig M. Stimmel <cstimmel@uw.edu>
2017-02-24 14:24:25 -0800

    Fixed phpcs errors and beautify.

--- setup/class.isc-admin-corner-links.php
+++ setup/class.isc-admin-corner-links.php
@@ -1,21 +1,32 @@
 <?php
-
 /**
- * ISC Admin Corner links
+ * ISC_AdminCornerLinks
+ *
  * This installs the front page quicklinks location.
+ *
+ * @package isc-uw-child
  */
 
+/**
+ * ISC_AdminCornerLinks
+ */
 class ISC_AdminCornerLinks {
 
 
 	const NAME              = 'Admin Corner Links';
 	const LOCATION       = 'admin-corner-links';
 
+	/**
+	 * Constructor method
+	 */
 	function __construct() {
 		$this->menu_items = array();
 		add_action( 'after_setup_theme', array( $this, 'register_admin_corner_links' ) );
 	}
 
+	/**
+	 * Registration method
+	 */
 	function register_admin_corner_links() {
 		register_nav_menu( self::LOCATION, __( self::NAME ) );
 	}
