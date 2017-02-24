c141eabdb8ca5f7a9dff5304679e6181305dfabf
Craig M. Stimmel <cstimmel@uw.edu>
2017-02-24 12:07:05 -0800

    Beautify with PHPCBF.

--- setup/class.isc-admin-corner-links.php
+++ setup/class.isc-admin-corner-links.php
@@ -5,21 +5,19 @@
  * This installs the front page quicklinks location.
  */
 
-class ISC_AdminCornerLinks
-{
+class ISC_AdminCornerLinks {
 
-    const NAME              = 'Admin Corner Links';
-    const LOCATION       = 'admin-corner-links';
 
-    function __construct()
-    {
-        $this->menu_items = array();
-        add_action('after_setup_theme', array( $this, 'register_admin_corner_links'));
-    }
+	const NAME              = 'Admin Corner Links';
+	const LOCATION       = 'admin-corner-links';
 
-    function register_admin_corner_links()
-    {
-        register_nav_menu(self::LOCATION, __(self::NAME));
-    }
+	function __construct() {
+		$this->menu_items = array();
+		add_action( 'after_setup_theme', array( $this, 'register_admin_corner_links' ) );
+	}
+
+	function register_admin_corner_links() {
+		register_nav_menu( self::LOCATION, __( self::NAME ) );
+	}
 
 }
