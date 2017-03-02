99900b3aa69079925043af87efbd4cef94d4d305
Kevin Zhao <zhaok24@uw.edu>
2017-03-02 22:26:45 +0000

    fixed regex for urls, fixed telephone href

--- functions.php
+++ functions.php
@@ -141,9 +141,8 @@ function section_cb() {
 
 function display_map() {
     $options = get_option('footer_fields');
-    $url_pattern = '(http|https|ftp):\/\/[a-zA-Z0-9_\-\.\+]+\.[a-zA-Z0-9]+([\/a-zA-z0-9_\-\.\+\?\/=%@\/\,]*)?';
+    $url_pattern = '(http|https|ftp):\/\/[a-zA-Z0-9_\-\.\+]+\.[a-zA-Z0-9]+([\/a-zA-z0-9_\-\.\+\?=%@,!:]*)?';
     $warning = 'Example: http://example.com/page';
-    log_to_console(wp_load_alloptions());
     echo "<input name='footer_fields[map]' pattern='$url_pattern' title='$warning' type='text' size='45' value='{$options['map']}' />";
 }
 
@@ -151,7 +150,6 @@ function display_location() {
     $options = get_option('footer_fields');
     $url_pattern = '.*';
     $warning = 'Example: UW Tower';
-    log_to_console($options);
     echo "<input name='footer_fields[location]' pattern='$url_pattern' title='$warning' type='text' size='45' value='{$options['location']}' />";
 }
 
@@ -159,8 +157,6 @@ function display_email() {
     $options = get_option('footer_fields');
     $email_pattern = '[a-zA-Z0-9_\.\-]+@([a-zA-Z0-9_\.\-]+\.[a-zA-Z0-9]+)';
     $warning = 'Example: user@uw.edu';
-    log_to_console(wp_load_alloptions());
-
     echo "<input name='footer_fields[email]' pattern='$email_pattern' title='$warning'type='text' value='{$options['email']}' />";
 }
 
