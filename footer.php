b962da060fdefc106c4e74211e32c9b0722a28ae
Charlon Palacay <charlon@gmail.com>
2017-03-08 11:30:59 -0800

    Fixes footer contact links styles.

--- footer.php
+++ footer.php
@@ -18,16 +18,17 @@
 	<div class="container isc-footer">
 		<div class="row">
 
-			<div class="col-md-5">
+            <?php $options = get_option( 'isc_footer_fields' );?>
+
+			<div class="col-md-6">
 			  <h3>Contact Information</h3>
 			  <div class="row  location-container">
 			  <div class="col-md-6 map-container">
-				 <a href="https://goo.gl/maps/koRTixJW8D62" title="ISC location (Google Maps)"><img src="<?php echo esc_url( get_site_url() ) . '/wp-content/themes//isc-uw-child/assets/images/map.png'?>" alt="Map of ISC location" ></a>
+				 <a href="<?php echo $options['map'] ?>" title="<?php echo $options['location'] ?>"><img src="<?php echo esc_url( get_site_url() ) . '/wp-content/themes//isc-uw-child/assets/images/map.png'?>" alt="Map of ISC location" ></a>
 			  </div>
 			  <div class="col-md-6">
 				<ul class="footer-contact-container">
-					<?php $options = get_option( 'isc_footer_fields' );?>
-				  <li><div class="icon-container"><i class="fa fa-map-marker" aria-hidden="true"></i></div><div class="text-container location-text"><a href="<?php echo $options['map'] ?>">   <?php echo $options['location'] ?></a></div></li>
+				  <li><div class="icon-container"><i class="fa fa-map-marker" aria-hidden="true"></i></div><div class="text-container"><a href="<?php echo $options['map'] ?>">   <?php echo $options['location'] ?></a></div></li>
 				  <li><div class="icon-container"><i class="fa fa-phone-square " aria-hidden="true"></i></div><div class="text-container"><a href="<?php echo 'tel:1-' . $options['phone'] ?>"> <?php echo $options['phone'] ?>   </a></div></li>
 				  <li><div class="icon-container icons-stacked"><i class="fa fa-square fa-stack-2x" aria-hidden="true"></i><i class="fa fa-envelope fa-stack-1x" aria-hidden="true"></i></div><div class="text-container"><a href="<?php echo 'mailto:' . $options['email'] ?>"> <?php echo $options['email']?> </a></div></li>
 				</ul>
@@ -35,7 +36,7 @@
 			</div>
 		  </div>
 
-			<div class="col-md-7">
+			<div class="col-md-6">
 				<div class="row">
 					<?php
 					wp_nav_menu(
