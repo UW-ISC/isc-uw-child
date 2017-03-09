71ecd1671d2b8cd56b321f9ca4ebe07ee1602174
mmgionet <mgionet@uw.edu>
2017-03-09 11:26:41 -0800

    Adding comments and fixing minor style issues

--- setup/class.uw-scripts.php
+++ setup/class.uw-scripts.php
@@ -1,203 +1,214 @@
 <?php
 /**
+ * Unique template for Admin Corner page
  * This is where all the JS files are registered
  *    - bloginfo('template_directory')  gives you the url to the parent theme
  *    - bloginfo('stylesheet_directory')  gives you the url to the child theme
+ *
+ * @package isc-uw-child
+ * @author UW-IT AXDD
  */
+class UW_Scripts {
 
-class UW_Scripts
-{
+	/**
+	 * @var $SCRIPTS defines ..
+	 */
+	public $SCRIPTS;
 
-    public $SCRIPTS;
+	/**
+	 * This method constructs the scripts of the theme
+	 */
+	function __construct() {
 
+		$multi = is_multisite();
+		$parent = get_bloginfo( 'template_url' );
+		$child = get_stylesheet_directory_uri();
 
-    function __construct()
-    {
+		$this->SCRIPTS = array_merge(
+			array(
 
-        $multi = is_multisite();
-        $parent = get_bloginfo('template_url');
-        $child = get_stylesheet_directory_uri();
+			'jquery' => array(
+			'id'      => 'jquery',
+			'url'     => 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js',
+			'deps'    => array(),
+			'version' => '1.12.4',
+			'admin'   => false,
+			),
 
-        $this->SCRIPTS = array_merge(
-            array(
+			'site'   => array(
+			'id'        => 'site',
+			'url'       => $parent . '/js/site' . $this->dev_script() . '.js',
+			'deps'      => array( 'backbone' ),
+			'version'   => '1.0.3',
+			'admin'     => false,
+			'style_dir' => site_url(),
+			),
 
-            'jquery' => array (
-            'id'      => 'jquery',
-            'url'     => 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js',
-            'deps'    => array(),
-            'version' => '1.12.4',
-            'admin'   => false
-            ),
+			'admin' => array(
+			'id'      => 'wp.admin',
+			'url'     => $parent . '/assets/admin/js/admin.js',
+			'deps'    => array( 'jquery' ),
+			'version' => '1.0',
+			'admin'   => true,
+			),
 
-            'site'   => array (
-            'id'        => 'site',
-            'url'       => $parent . '/js/site' . $this->dev_script() . '.js',
-            'deps'      => array( 'backbone' ),
-            'version'   => '1.0.3',
-            'admin'     => false,
-            'style_dir' => site_url()
-            // 'variables' => array( 'is_multisite' =>  $multi ),
-            ),
+			'sticky' => array(
+			'id'      => 'sticky',
+			'url'     => $child . '/assets/js/sticky.js',
+			'deps'    => array( 'jquery' ),
+			'version' => '1.0',
+			'admin'   => false,
+			),
 
-            'admin' => array (
-            'id'      => 'wp.admin',
-            'url'     => $parent . '/assets/admin/js/admin.js',
-            'deps'    => array( 'jquery' ),
-            'version' => '1.0',
-            'admin'   => true
-            ),
+			'jquery-datatables' => array(
+			'id'      => 'jquery-datatables',
+			'url'     => 'https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js',
+			'deps'    => array( 'jquery' ),
+			'version' => '1.10.13',
+			'admin'   => false,
+			),
 
-            'sticky' => array (
-            'id'      => 'sticky',
-            'url'     => $child . '/assets/js/sticky.js',
-            'deps'    => array( 'jquery' ),
-            'version' => '1.0',
-            'admin'   => false
-            ),
+			'datatables-bootstrap' => array(
+			'id'      => 'datatables-bootstrap',
+			'url'     => 'https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js',
+			'deps'    => array( 'jquery' ),
+			'version' => '1.10.13',
+			'admin'   => false,
+			),
 
-            'jquery-datatables' => array (
-            'id'      => 'jquery-datatables',
-            'url'     => 'https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js',
-            'deps'    => array( 'jquery' ),
-            'version' => '1.10.13',
-            'admin'   => false
-            ),
+			'bootstrap-collapse' => array(
+			'id'      => 'bootstrap-collapse',
+			'url'     => $child . '/vendor/js/bootstrap-collapse.js',
+			'deps'    => array( 'jquery' ),
+			'version' => '2.0.4',
+			'admin'   => false,
+			),
 
-            'datatables-bootstrap' => array (
-            'id'      => 'datatables-bootstrap',
-            'url'     => 'https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js',
-            'deps'    => array( 'jquery' ),
-            'version' => '1.10.13',
-            'admin'   => false
-            ),
+			'jquery-succinct' => array(
+			'id'      => 'jquery-succinct',
+			'url'     => $child . '/vendor/js/jquery.succinct.min.js',
+			'deps'    => array( 'jquery' ),
+			'version' => '1.1.0',
+			'admin'   => false,
+			),
 
-            'bootstrap-collapse' => array (
-            'id'      => 'bootstrap-collapse',
-            'url'     => $child . '/vendor/js/bootstrap-collapse.js',
-            'deps'    => array( 'jquery' ),
-            'version' => '2.0.4',
-            'admin'   => false
-            ),
+			'isc-js' => array(
+			'id'      => 'isc-js',
+			'url'     => $child . '/assets/js/isc.js',
+			'deps'    => array(),
+			'version' => '1.0.0',
+			'admin'   => false,
+			),
 
-            'jquery-succinct' => array (
-            'id'      => 'jquery-succinct',
-            'url'     => $child . '/vendor/js/jquery.succinct.min.js',
-            'deps'    => array( 'jquery' ),
-            'version' => '1.1.0',
-            'admin'   => false
-            ),
+			), $this->get_child_theme_scripts()
+		);
 
-            'isc-js' => array (
-            'id'      => 'isc-js',
-            'url'     => $child .'/assets/js/isc.js',
-            'deps'    => array(),
-            'version' => '1.0.0',
-            'admin'   => false
-            ),
+		add_action( 'wp_enqueue_scripts', array( $this, 'uw_register_default_scripts' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'uw_localize_default_scripts' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'uw_enqueue_default_scripts' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'uw_enqueue_admin_scripts' ) );
+		add_action( 'customize_controls_init', array( $this, 'uw_customizer_preview' ) );
 
-            ), $this->get_child_theme_scripts()
-        );
+	}
 
-        add_action('wp_enqueue_scripts', array( $this, 'uw_register_default_scripts' ));
-        add_action('wp_enqueue_scripts', array( $this, 'uw_localize_default_scripts' ));
-        add_action('wp_enqueue_scripts', array( $this, 'uw_enqueue_default_scripts' ));
-        add_action('admin_enqueue_scripts', array( $this, 'uw_enqueue_admin_scripts' ));
-        add_action('customize_controls_init', array( $this, 'uw_customizer_preview' ));
+	/**
+	 * This function creates a customizer preview (??)
+	 */
+	function uw_customizer_preview() {
+		wp_enqueue_script( 'uw-themecustomize', get_bloginfo( 'template_directory' ) . '/js/uw.themecustomizer.js', array( 'jquery', 'customize-controls' ), false, true );
+	}
 
-    }
+	/**
+	 * This functions reverts the scripts to their default
+	 * condition
+	 */
+	function uw_register_default_scripts() {
+		wp_deregister_script( 'jquery' );
 
-    function uw_customizer_preview()
-    {
-        // wp_enqueue_script(
-        //     'uw-themecustomizer',      //ID
-        //     get_bloginfo('template_directory') .'/js/uw.themecustomizer.js',//URL
-        //     array( 'jquery','customize-preview' ),  //dependencies
-        //     '',           //version (optional)
-        //     true            //Put script in footer?
-        // );
-        wp_enqueue_script('uw-themecustomize', get_bloginfo('template_directory') .'/js/uw.themecustomizer.js', array( 'jquery', 'customize-controls' ), false, true);
-    }
+		foreach ( $this->SCRIPTS as $script ) {
+			$script = (object) $script;
 
-    function uw_register_default_scripts()
-    {
-        wp_deregister_script('jquery');
+			wp_register_script(
+				$script->id,
+				$script->url,
+				$script->deps,
+				$script->version
+			);
 
-        foreach ( $this->SCRIPTS as $script )
-        {
-            $script = (object) $script;
+		}
 
-            wp_register_script(
-                $script->id,
-                $script->url,
-                $script->deps,
-                $script->version
-            );
+	}
 
-        }
+	/**
+	 * This function localizes the default scripts
+	 * with data for a javascript variable
+	 */
+	function uw_localize_default_scripts() {
+		$uw_localization = array();
+		foreach ( $this->SCRIPTS as $script ) {
+			$script = (object) $script;
+			if ( isset( $script->style_dir ) ) {
+				wp_localize_script( $script->id, 'style_dir', $script->style_dir ); // error line.
+			}
+		}
+	}
 
-    }
+	/**
+	 * This function registers and enqueues
+	 * the default scripts
+	 */
+	function uw_enqueue_default_scripts() {
+		foreach ( $this->SCRIPTS as $script ) {
+			$script = (object) $script;
 
-    function uw_localize_default_scripts()
-    {
-        $uw_localization = array();
-        foreach ($this->SCRIPTS as $script )
-        {
-            $script = (object) $script;
-            // if (isset($script->variables)){
-            //   wp_localize_script($script->id, 'uw_ismultisite', $script->variables); //error line
-            // }
-            if (isset($script->style_dir)) {
-                wp_localize_script($script->id, 'style_dir', $script->style_dir); //error line
-            }
-        }
-    }
+			if ( ! $script->admin ) {
+				wp_enqueue_script( $script->id );
+			}
+		}
+	}
 
-    function uw_enqueue_default_scripts()
-    {
-        foreach ( $this->SCRIPTS as $script )
-        {
-            $script = (object) $script;
+	/**
+	 * If user is logged in as an admin this will
+	 * register and enqueue all scripts
+	 */
+	function uw_enqueue_admin_scripts() {
+		if ( ! is_admin() ) {
+			return;
+		}
 
-            if (! $script->admin ) {
-                wp_enqueue_script($script->id);
-            }
-        }
-    }
+		foreach ( $this->SCRIPTS as $script ) {
+			$script = (object) $script;
 
-    function uw_enqueue_admin_scripts()
-    {
-        if (! is_admin() ) {
-            return;
-        }
+			if ( $script->admin ) {
 
-        foreach ( $this->SCRIPTS as $script )
-        {
-            $script = (object) $script;
+				wp_register_script(
+					$script->id,
+					$script->url,
+					$script->deps,
+					$script->version
+				);
 
-            if ($script->admin ) {
+				wp_enqueue_script( $script->id );
 
-                wp_register_script(
-                    $script->id,
-                    $script->url,
-                    $script->deps,
-                    $script->version
-                );
+			}
+		}
 
-                wp_enqueue_script($script->id);
+	}
 
-            }
-        }
+	/**
+	 * This function returns scripts unique
+	 * to the child theme
+	 */
+	private function get_child_theme_scripts() {
+		return is_array( $this->SCRIPTS ) ? $this->SCRIPTS : array();
+	}
 
-    }
-
-    private function get_child_theme_scripts()
-    {
-        return is_array($this->SCRIPTS) ? $this->SCRIPTS : array();
-    }
-
-    public function dev_script()
-    {
-        return is_user_logged_in() ? '.dev' : '';
-    }
+	/**
+	 * This function returns true if dev user
+	 * is logged in, otherwise false
+	 */
+	public function dev_script() {
+		return is_user_logged_in() ? '.dev' : '';
+	}
 
 }
