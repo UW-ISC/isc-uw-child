335ad79e8eaf0aeb439bc77390c68575c4bf0c69
Kevin <zhaok24@uw.edu>
2017-04-07 20:15:24 +0000

    Revert "Revert "fixes ISCWEB-120 by escaping the html. Made a function for it and put it in every template""
    This reverts commit 3a250c1369150601ae33066f9b9232fdcf339b13.

--- templates/template-article-page.php
+++ templates/template-article-page.php
@@ -31,7 +31,7 @@ get_header();
             <?php
 
             while ( have_posts() ) : the_post();
-                the_title('<h2 class="title">', '</h2>');
+                isc_title();
                 the_content();
             endwhile;
 
