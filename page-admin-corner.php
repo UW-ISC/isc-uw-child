4007a629ca470d3ca4e2aa2d8d39cd21f98d92b1
mmgionet <mgionet@uw.edu>
2017-04-11 15:15:10 -0700

    Revert "Fixes ISCWEB-129 by limiting the seasonal topics that display on the admin corner to only one"
    This reverts commit d80d341d51a396fcb650aa76533ee33d2beae0d5.

--- page-admin-corner.php
+++ page-admin-corner.php
@@ -173,14 +173,12 @@ get_header();
 					 'post_status'  => 'publish',
 					 'meta_key'     => 'isc-featured',
 					 'meta_value'   => 'seasonal',
-					 'posts_per_page' => 1,
 					);
 					$seasonal_featured = get_pages( $args );
 					if ( ! $seasonal_featured ) {
 						echo '<p>No featured seasonal topics found.</p>';
 					} else {
-						for ($i = 0; $i < 1; $i++) {
-							$featured_page = $seasonal_featured[$i];
+						foreach ( $seasonal_featured as $featured_page ) {
 							$html = '<h4><a href="' . get_post_permalink( $featured_page->ID ) . '">' . get_the_title( $featured_page->ID ) . '</a></h4>';
 							$html .= "<p style='margin-bottom:1.5em;'>";
 							$custom = get_post_custom( $featured_page->ID );
