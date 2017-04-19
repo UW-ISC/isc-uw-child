962f1752237c2954c940ce4512fda7899ab0d71d
Charlon Palacay <charlon@gmail.com>
2017-04-19 10:42:41 -0700

    Fixes tabbing functionality of toc and fixes aria-expanded behavior. Resolves ISCWEB-107

--- inc/user-guide-functions.php
+++ inc/user-guide-functions.php
@@ -255,11 +255,11 @@ if ( ! function_exists( 'isc_user_guide_menu' ) ) :
 
 			if ( count( $cur->subheaders ) > 0 ) {
 				// This means there are subheaders under the current header.
-				$pages .= '<li class="nav-item has-children" aria-expanded="false"> <a class="nav-link children-toggle collapsed" role="button" data-toggle="collapse" data-target="#' . $slug . '" aria-controls="#' . $slug . '" aria-expanded="false">' . $name . '<i class="fa fa-2x"></i></a>';
+				$pages .= '<li class="nav-item has-children"><a href="javascript:void();" class="nav-link children-toggle collapsed" role="button" data-toggle="collapse" data-target="#' . $slug . '" aria-controls="#' . $slug . '" aria-expanded="false">' . $name . '<i class="fa fa-2x"></i></a>';
 				$pages .= '<ul class="children depth-1 collapse" id="' . $slug . '" style="height: 0px;">';
 				// Iterate through the subheaders under the current header.
 				foreach ( $cur->subheaders as $subname => $subslug ) {
-					$pages .= '<li class="nav-item"> <a class="nav-link" title="' . stripslashes( $subname ) . '" href="#' . $subslug . '">' . stripslashes( $subname ) . '</a></li>';
+					$pages .= '<li class="nav-item"><a class="nav-link" title="' . stripslashes( $subname ) . '" href="#' . $subslug . '">' . stripslashes( $subname ) . '</a></li>';
 				}
 				$pages .= '</ul></li>';
 			} else {
