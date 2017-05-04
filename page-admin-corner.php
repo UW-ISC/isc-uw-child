5763a5b4ada5b629815ab983cb5d3fe26975eff0
Kevin <zhaok24@uw.edu>
2017-05-04 18:30:25 +0000

    removing paragraph tags from the excerpt

--- page-admin-corner.php
+++ page-admin-corner.php
@@ -86,10 +86,7 @@ get_header();
 
 								   <h4><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title() ?></a></h4>
 								   <div class="update-date"><?php echo get_the_date() ?> </div>
-										<?php
-										$excerpt = strip_tags( the_excerpt(), '<p><a><strong><b><em><i>' );
-										?>
-								   <div class='post-content'><?php echo $excerpt; ?></div>
+								   <div class='post-content'><?php the_excerpt() ?></div>
 							<?php
 								 endwhile;
 								?>
