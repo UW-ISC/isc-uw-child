ed9485785af9ef9c3930b51db743344ed459642a
Charlon Palacay <charlon@gmail.com>
2017-05-11 11:07:53 -0700

    Adds main_content id on selected templates. Resolves ISCWEB-177

--- templates/template-article-section.php
+++ templates/template-article-section.php
@@ -23,7 +23,7 @@ get_header();
 
     <div class="row">
 
-        <article class="uw-body-copy col-md-9">
+        <article class="uw-body-copy col-md-9" id="main_content">
 
             <?php log_to_console("template-article-section.php") ?>
 
