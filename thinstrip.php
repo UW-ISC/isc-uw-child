a1cfc6ff63dea967caa0ecac28702b5e20d6826a
Craig M. Stimmel <cstimmel@uw.edu>
2017-06-27 11:51:38 -0700

    Add dummy alert.

--- thinstrip.php
+++ thinstrip.php
@@ -63,3 +63,5 @@
 	  </div>
 
 </header>
+<div id="uwalert-alert-message" class=" advisories  uwalert-steel "><div class="container"><span class="close">Close</span><h1>Armed robbery near U-Village</h1><p></p><p>An armed robbery occurred about 10:30 a.m. at the Safeway in the 3000 block of 45th St. near University Village. The suspect is described as a black male, 20-30 years-old, wearing a dark blue hoodie with white Nike lettering and blue jeans. Call 911 if seen.</p>
+<a class="more" href="http://emergency.uw.edu" title="Armed robbery near U-Village">More info</a><p></p></div></div>
