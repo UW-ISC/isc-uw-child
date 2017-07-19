8f6f798b33e022ecce867a34b568573a13a00f3a
Craig M. Stimmel <cstimmel@uw.edu>
2017-07-19 13:11:39 -0700

    Use the excerpt for alert banner once custom trim filter is fixed.

--- thinstrip.php
+++ thinstrip.php
@@ -75,14 +75,15 @@ $alert_args = array(
 $alert_news_posts = get_posts( $alert_args );
 
 if ( $alert_news_posts ) :
-	$alert_news_post = $alert_news_posts[0] ?>
+	$alert_news_post = get_post( $alert_news_posts[0]->ID ) ?>
+    <?php setup_postdata( $GLOBALS['post'] =& $alert_news_post ); ?>
 	<div id="uwalert-alert-message" class="advisories  uwalert-steel">
 		<div class="container">
 			<h1><?php echo get_the_title( $alert_news_post ); ?></h1>
 			<p></p>
-			<p><?php echo wp_strip_all_tags( get_post_field( 'post_content', $alert_news_post->ID ) ); ?></p>
+            <?php the_excerpt(); ?>
+            <?php wp_reset_postdata() ?>
 			<p></p>
-			<a class="more" href="<?php echo esc_url( get_post_permalink( $alert_news_post->ID ) ); ?>" title="More Info">More Info</a>
 		</div>
 	</div>
 <?php endif; ?>
