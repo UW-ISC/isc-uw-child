d5685a927507b8558f5864f071ac1c7f13cdfb54
Craig M. Stimmel <cstimmel@uw.edu>
2017-07-20 15:32:31 -0700

    Fix whitespace with phpcbf.

--- page.php
+++ page.php
@@ -12,49 +12,49 @@ get_header();
 
 <div role="main">
 
-    <?php uw_site_title(); ?>
-    <?php get_template_part( 'menu', 'mobile' ); ?>
+	<?php uw_site_title(); ?>
+	<?php get_template_part( 'menu', 'mobile' ); ?>
 
-    <div class="container uw-body">
+	<div class="container uw-body">
 
-    	<div class="row">
-    		<div class="col-md-12">
+		<div class="row">
+			<div class="col-md-12">
 
-    			<?php get_template_part( 'breadcrumbs' ); ?>
-    		</div>
-    	</div>
+				<?php get_template_part( 'breadcrumbs' ); ?>
+			</div>
+		</div>
 
-    	<div class="row">
+		<div class="row">
 
-    		<div class="uw-content col-md-9">
+			<div class="uw-content col-md-9">
 
-    			<div id='main_content' class="uw-body-copy" tabindex="-1">
+				<div id='main_content' class="uw-body-copy" tabindex="-1">
 
-    				<?php log_to_console( 'page.php' ) ?>
+					<?php log_to_console( 'page.php' ) ?>
 
-    				<?php isc_title(); ?>
+					<?php isc_title(); ?>
 
-    				<?php
-    				// Start the Loop.
-    				while ( have_posts() ) : the_post();
+					<?php
+					// Start the Loop.
+					while ( have_posts() ) : the_post();
 
-    					the_content();
+						the_content();
 
-    					// If comments are open or we have at least one comment, load up the comment template.
-    					if ( comments_open() || get_comments_number() ) {
-    						  comments_template();
-    					}
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) {
+							  comments_template();
+						}
 
-    				endwhile;
-    				?>
+					endwhile;
+					?>
 
-    			</div>
+				</div>
 
-    		</div>
+			</div>
 
-    	</div>
+		</div>
 
-    </div>
+	</div>
 
 </div>
 
