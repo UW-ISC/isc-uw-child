78d7b47ff1b12d2d74209579556cbaa6b99973d4
Craig M. Stimmel <cstimmel@uw.edu>
2017-07-20 15:31:38 -0700

    Fix whitespace with phpcbf.

--- page-user-guides.php
+++ page-user-guides.php
@@ -16,132 +16,132 @@ get_header();
 
 		<?php $user_guides = isc_get_user_guides(); // grabs all the user guides. ?>
 
-    <div class="container uw-body">
+	<div class="container uw-body">
 
-    	<div class="row">
-    		<div class="col-md-12">
-    			<?php get_template_part( 'breadcrumbs' ); ?>
-    		</div>
-    	</div>
+		<div class="row">
+			<div class="col-md-12">
+				<?php get_template_part( 'breadcrumbs' ); ?>
+			</div>
+		</div>
 
 
-    	<div class="row">
-    		<div class="col-md-12" id="main_content">
+		<div class="row">
+			<div class="col-md-12" id="main_content">
 
-                <?php isc_title(); ?>
+				<?php isc_title(); ?>
 
-                <h2 class="sr-only">Search Options</h2>
-    			<div style="background:#f9f9f9; padding: 20px; margin-bottom:1em; font-size: 14px;">
+				<h2 class="sr-only">Search Options</h2>
+				<div style="background:#f9f9f9; padding: 20px; margin-bottom:1em; font-size: 14px;">
 
-    				<div class="row">
+					<div class="row">
 
-    					<div class="col-md-8">
-    						<div class="sr-only">Filter by:</div>
-    						<div class="row">
-    							<div class="col-md-6">
-    								<label>Topic:</label>
-    								<select class="form-control input-sm" id="topic-dropdown">
-    								  <option value="---"> --- </option>
-    									<?php
-    									  $topics = (isc_get_all_topics( $user_guides ));
-    									foreach ( $topics as $topic ) {
-    										echo '<option value = "' . esc_attr( sanitize_title( $topic ) ) . '"> ' . esc_html( $topic ) . ' </option>';
-    									}
-    									?>
-    								</select>
-    							</div>
-    							<div class="col-md-6">
-    								<label>Security Role:</label>
-    								<select class="form-control input-sm" id="role-dropdown">
-    								  <option value="---"> --- </option>
-    									<?php
-    									  $roles = (isc_get_all_roles( $user_guides ));
-    									foreach ( $roles as $role ) {
-    										echo '<option value = "' . esc_attr( sanitize_title( $role ) ) . '"> ' . esc_html( $role ) . ' </option>';
-    									}
-    									?>
-    								</select>
-    							</div>
-    						</div>
-    					</div>
-    					<div class="col-md-4">
-    						<div class="sr-only">Search by:</div>
-    						<label>Keyword:</label>
-    						<input class="form-control input-sm" type="text" id="myInputTextField">
-    					</div>
-    				</div>
+						<div class="col-md-8">
+							<div class="sr-only">Filter by:</div>
+							<div class="row">
+								<div class="col-md-6">
+									<label>Topic:</label>
+									<select class="form-control input-sm" id="topic-dropdown">
+									  <option value="---"> --- </option>
+										<?php
+										  $topics = (isc_get_all_topics( $user_guides ));
+										foreach ( $topics as $topic ) {
+											echo '<option value = "' . esc_attr( sanitize_title( $topic ) ) . '"> ' . esc_html( $topic ) . ' </option>';
+										}
+										?>
+									</select>
+								</div>
+								<div class="col-md-6">
+									<label>Security Role:</label>
+									<select class="form-control input-sm" id="role-dropdown">
+									  <option value="---"> --- </option>
+										<?php
+										  $roles = (isc_get_all_roles( $user_guides ));
+										foreach ( $roles as $role ) {
+											echo '<option value = "' . esc_attr( sanitize_title( $role ) ) . '"> ' . esc_html( $role ) . ' </option>';
+										}
+										?>
+									</select>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="sr-only">Search by:</div>
+							<label>Keyword:</label>
+							<input class="form-control input-sm" type="text" id="myInputTextField">
+						</div>
+					</div>
 
-    			</div>
+				</div>
 
-    			<h2 class="sr-only">User Guides</h2>
+				<h2 class="sr-only">User Guides</h2>
 
-    			<table id="user_guide_lib" class="table table-striped" style="border:none !important;">
-    				<thead style="font-size:14px;">
-    					<tr>
-    						<th>User Guide</th>
-    						<th>Topic</th>
-    						<th>Security Role</th>
-    					</tr>
-    				</thead>
-    				<tbody>
-    					<?php isc_user_guide_table( $user_guides ); ?>
-    				</tbody>
+				<table id="user_guide_lib" class="table table-striped" style="border:none !important;">
+					<thead style="font-size:14px;">
+						<tr>
+							<th>User Guide</th>
+							<th>Topic</th>
+							<th>Security Role</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php isc_user_guide_table( $user_guides ); ?>
+					</tbody>
 
-    			</table>
+				</table>
 
-    			<script type="text/javascript" charset="utf-8">
-    			var table;
-    			$(document).ready(function() {
-    				table = $('#user_guide_lib').DataTable( {
-    					"paging":   false,
-    					"order": [[ 0, "asc" ]], // order user guide list by user guide name (newest on top)
-    				});
+				<script type="text/javascript" charset="utf-8">
+				var table;
+				$(document).ready(function() {
+					table = $('#user_guide_lib').DataTable( {
+						"paging":   false,
+						"order": [[ 0, "asc" ]], // order user guide list by user guide name (newest on top)
+					});
 
-    				$('#myInputTextField').keyup(function(){
-    					  table.search($(this).val()).draw() ;
-    				});
+					$('#myInputTextField').keyup(function(){
+						  table.search($(this).val()).draw() ;
+					});
 
-    			});
-    			$("#topic-dropdown, #role-dropdown").change(function() {
-    				var topic_value = $("#topic-dropdown").val();
-    				var role_value = $("#role-dropdown").val();
-    				var user_guides = $("[id=user-guide]");
-    				$.fn.dataTable.ext.search.pop();
-    				$.fn.dataTable.ext.search.push(
-    				  function(settings, data, dataIndex) {
-    					var topics = $(table.row(dataIndex).node()).attr('data-topics');
-    					var roles = $(table.row(dataIndex).node()).attr('data-roles');
-    					if (topics == undefined) {
-    					  topics = "";
-    					}
-    					if (roles == undefined) {
-    					  roles = "";
-    					}
-    					topics = topics.split(" ");
-    					roles = roles.split(" ");
-    					if (role_value == "---" && topic_value == "---") {
-    					  // return everything
-    					  return true;
-    					} else if (role_value == "---") {
-    					  // filter by topic
-    					  return topics.indexOf(topic_value) != -1;
-    					} else if (topic_value == "---") {
-    					  // filter by role
-    					  return roles.indexOf(role_value) != -1;
-    					} else {
-    					  // filter by both
-    					  return topics.indexOf(topic_value) != -1 && roles.indexOf(role_value) != -1;
-    					}
-    				  }
-    				);
-    				table.draw();
-    			});
-    			</script>
+				});
+				$("#topic-dropdown, #role-dropdown").change(function() {
+					var topic_value = $("#topic-dropdown").val();
+					var role_value = $("#role-dropdown").val();
+					var user_guides = $("[id=user-guide]");
+					$.fn.dataTable.ext.search.pop();
+					$.fn.dataTable.ext.search.push(
+					  function(settings, data, dataIndex) {
+						var topics = $(table.row(dataIndex).node()).attr('data-topics');
+						var roles = $(table.row(dataIndex).node()).attr('data-roles');
+						if (topics == undefined) {
+						  topics = "";
+						}
+						if (roles == undefined) {
+						  roles = "";
+						}
+						topics = topics.split(" ");
+						roles = roles.split(" ");
+						if (role_value == "---" && topic_value == "---") {
+						  // return everything
+						  return true;
+						} else if (role_value == "---") {
+						  // filter by topic
+						  return topics.indexOf(topic_value) != -1;
+						} else if (topic_value == "---") {
+						  // filter by role
+						  return roles.indexOf(role_value) != -1;
+						} else {
+						  // filter by both
+						  return topics.indexOf(topic_value) != -1 && roles.indexOf(role_value) != -1;
+						}
+					  }
+					);
+					table.draw();
+				});
+				</script>
 
-    		</div>
-    	</div>
+			</div>
+		</div>
 
-    </div>
+	</div>
 
 </div>
 
