3af324cfee0d6f2974b10a8b69a563c8ad1cbd09
Craig M. Stimmel <cstimmel@uw.edu>
2017-07-20 15:39:35 -0700

    Fix whitespace with phpcbf.

--- templates/template-article-page.php
+++ templates/template-article-page.php
@@ -13,55 +13,55 @@ get_header();
 
 <div role="main">
 
-    <?php uw_site_title(); ?>
-    <?php get_template_part('menu', 'mobile'); ?>
+	<?php uw_site_title(); ?>
+	<?php get_template_part( 'menu', 'mobile' ); ?>
 
-    <section class="uw-body container">
+	<section class="uw-body container">
 
-        <div class="row">
-            <div class="col-md-12">
-                <?php get_template_part('breadcrumbs'); ?>
-            </div>
-        </div>
+		<div class="row">
+			<div class="col-md-12">
+				<?php get_template_part( 'breadcrumbs' ); ?>
+			</div>
+		</div>
 
-        <div class="row" id="top">
+		<div class="row" id="top">
 
-            <article id='main_content' class="uw-body-copy col-md-9" tabindex="-1" id="main_content">
+			<article id='main_content' class="uw-body-copy col-md-9" tabindex="-1" id="main_content">
 
-                <?php log_to_console("template-article-page.php") ?>
+				<?php log_to_console( 'template-article-page.php' ) ?>
 
-                <?php
+				<?php
 
-                while ( have_posts() ) : the_post();
-                    isc_title();
-                    the_content();
-                endwhile;
+				while ( have_posts() ) : the_post();
+					isc_title();
+					the_content();
+				endwhile;
 
-                    isc_display_child_pages_with_toc();
-                ?>
+					isc_display_child_pages_with_toc();
+				?>
 
-            </article>
+			</article>
 
-            <script>
-            $(function(){
-                $('.isc-article-content table').each(function() {
-                    // add responsive table class and clear all other inline styles
-                    $(this).addClass("table-responsive");
-                    $(this).addClass("table-condensed");
-                    $(this).removeProp("style");
+			<script>
+			$(function(){
+				$('.isc-article-content table').each(function() {
+					// add responsive table class and clear all other inline styles
+					$(this).addClass("table-responsive");
+					$(this).addClass("table-condensed");
+					$(this).removeProp("style");
 
-                    // clean each td by removing all inline styles
-                    $("td").each(function() {
-                        $(this).removeProp("style");
-                    });
+					// clean each td by removing all inline styles
+					$("td").each(function() {
+						$(this).removeProp("style");
+					});
 
-                });
-            });
-            </script>
+				});
+			});
+			</script>
 
-        </div>
+		</div>
 
-    </section>
+	</section>
 
 </div>
 
