b8f1fc993f6e4b5eb94f507918199fa5b31bb89e
Craig M. Stimmel <cstimmel@uw.edu>
2017-07-20 15:41:28 -0700

    Fix whitespace with phpcbf.

--- templates/template-article-section.php
+++ templates/template-article-section.php
@@ -12,37 +12,37 @@ get_header();
 
 <div role="main">
 
-    <?php uw_site_title(); ?>
-    <?php get_template_part('menu', 'mobile'); ?>
+	<?php uw_site_title(); ?>
+	<?php get_template_part( 'menu', 'mobile' ); ?>
 
-    <section class="uw-body container">
+	<section class="uw-body container">
 
-        <div class="row">
-            <div class="col-md-12">
-                <?php get_template_part('breadcrumbs'); ?>
-            </div>
-        </div>
+		<div class="row">
+			<div class="col-md-12">
+				<?php get_template_part( 'breadcrumbs' ); ?>
+			</div>
+		</div>
 
-        <div class="row">
+		<div class="row">
 
-            <article class="uw-body-copy col-md-9" id="main_content">
+			<article class="uw-body-copy col-md-9" id="main_content">
 
-                <?php log_to_console("template-article-section.php") ?>
+				<?php log_to_console( 'template-article-section.php' ) ?>
 
-                <?php
-                while ( have_posts() ) : the_post();
-                    isc_title();
-                    // echoing the tags
-                    echo isc_get_tags(get_queried_object());
-                    the_content();
-                    edit_post_link();
-                endwhile;
+				<?php
+				while ( have_posts() ) : the_post();
+					isc_title();
+					// echoing the tags
+					echo isc_get_tags( get_queried_object() );
+					the_content();
+					edit_post_link();
+				endwhile;
 
-                ?>
-            </article>
+				?>
+			</article>
 
-        </div>
-    </section>
+		</div>
+	</section>
 
 </div>
 
