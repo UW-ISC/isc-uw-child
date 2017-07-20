2afcf39b24986d35a60bab8b15bded1eab48bc2a
Craig M. Stimmel <cstimmel@uw.edu>
2017-07-20 15:38:11 -0700

    Fix whitespace with phpcbf.

--- taxonomy-md-tags.php
+++ taxonomy-md-tags.php
@@ -11,49 +11,49 @@ get_header(); ?>
 
 <div role="main">
 
-    <?php uw_site_title(); ?>
-    <?php get_template_part( 'menu', 'mobile' ); ?>
+	<?php uw_site_title(); ?>
+	<?php get_template_part( 'menu', 'mobile' ); ?>
 
-    <div class="container uw-body">
+	<div class="container uw-body">
 
-    	<div class="row">
-    		<div class="col-md-12">
-    			<?php get_template_part( 'breadcrumbs' ); ?>
-    		</div>
-    	</div>
+		<div class="row">
+			<div class="col-md-12">
+				<?php get_template_part( 'breadcrumbs' ); ?>
+			</div>
+		</div>
 
-    	<div class="row">
+		<div class="row">
 
-        	<div class="uw-content col-md-9">
+			<div class="uw-content col-md-9">
 
-        		<div id='main_content' class="uw-body-copy" tabindex="-1">
+				<div id='main_content' class="uw-body-copy" tabindex="-1">
 
-        			<?php log_to_console( 'taxonomy-md-tags.php' ); ?>
+					<?php log_to_console( 'taxonomy-md-tags.php' ); ?>
 
-        			<h1><?php single_term_title() ?></h1>
+					<h1><?php single_term_title() ?></h1>
 
-        			<div>
-        				<?php
-        				if ( have_posts() ) :
-        					while ( have_posts() ) : the_post(); ?>
-        						<h2><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title() ?></a></h2>
-        						<div class='post-content'><?php the_excerpt() ?></div>
+					<div>
+						<?php
+						if ( have_posts() ) :
+							while ( have_posts() ) : the_post(); ?>
+								<h2><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title() ?></a></h2>
+								<div class='post-content'><?php the_excerpt() ?></div>
 
-        					<?php endwhile;
-        				  else :
-        						echo '<h2 class=\'no-results\'>Sorry, no results matched your criteria.</h2>';
-        				  endif; ?>
+							<?php endwhile;
+						  else :
+								echo '<h2 class=\'no-results\'>Sorry, no results matched your criteria.</h2>';
+						  endif; ?>
 
-        				<div><?php posts_nav_link( ' ' ); ?></div>
-        			</div>
+						<div><?php posts_nav_link( ' ' ); ?></div>
+					</div>
 
-        		</div>
+				</div>
 
-        	</div>
+			</div>
 
-    	</div>
+		</div>
 
-    </div>
+	</div>
 
 </div>
 
