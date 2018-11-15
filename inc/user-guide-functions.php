1a322bce943a8add02b6e810bccb42d306f03ce4
Prasad Thakur <prasadt@5CG6431T0V.clients.uw.edu>
2018-11-14 16:36:55 -0800

    0058 User Guide Feedback
    template-user-guide.php : Added 'Contact Form 7' shortcode for collecting User Guide Feedback.
    user-guide-functions.hp : Added Feedback list item in TOC to hint users towards this Feedback form.

--- inc/user-guide-functions.php
+++ inc/user-guide-functions.php
@@ -274,11 +274,13 @@ if ( ! function_exists( 'isc_user_guide_menu' ) ) :
 
 		// Add the title of the table of contents, this is the first element.
 		$first_li = $return ? '' : '<li class="nav-item"><a class="nav-link first" href="#top" title="Permanent Link to ' . get_bloginfo( 'name' ) . '"> Table of Contents </a></li>';
+		$last_li = $return ? '' : '<li class="nav-item"><a class="nav-link last" href="#feedback" title="Permanent Link to feedback form"> Feedback </a></li>';
 
 		$html = sprintf(
-			'<ul id="tocAccordion">%s%s</ul>',
+			'<ul id="tocAccordion">%s%s%s</ul>',
 			$first_li,
-			$pages
+			$pages,
+			$last_li
 		);
 
 		if ( empty( $pages ) ) {
