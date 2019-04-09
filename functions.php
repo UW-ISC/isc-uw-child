7459b0c73cf38073adc21916208cc0dee9f79101
prsdthkr <prasadt@uw.edu>
2019-04-09 16:04:47 -0700

    ISC-13306 Cards
    functions.php: added shortcode to support basic material cards with box shadow (supports card image, title, text description, and CTA link)
    isc-styles.css: styling changes

--- functions.php
+++ functions.php
@@ -492,4 +492,43 @@ function get_media_url_from_title($title){
 	return $attachment ? wp_get_attachment_url($attachment->ID) : '';
 }
 
+/**
+ * ISC card Shortcode
+ * This card will create a card (similar to a material design cards) with boxed shadow. 
+ * Cards can be arranged in responsive grids for better page navigation.
+ * This card has 4 sections:
+ * 	1. url: Image or Icon Url
+ * 	2. title: Card title text
+ * 	3. description (HTML enclosed with the shortcode): short excerpt or description of concept housed in this card.
+ * 	4. button_text: Label for the bottom call-to-action or link button.
+ *  5. button_link: Url for call-to-action button or link.
+ *  
+ * Usage eg.
+ * 	[card url="http://localhost/hrp-portal/wp-content/uploads/2019/02/wd-applet-career.png" title="Employee" button_text="Employee Training" button_link="https://isc.uw.edu/support-resources/workday-training/workday-training-for-employees/"]
+ *  I am looking for guidance to help me use Workday to add, make changes to, or take action on my own account.
+ *  [/card]
+ * @param string $atts Img Url, title, description, CTA label and CTA link attributes.
+ */
+function isc_card( $atts, $content= null ) {
+	extract(shortcode_atts(array(
+			'url' => '',
+			'title' => '',
+			'button_link' => '',
+			'button_text' => '',
+		),
+		$atts));
+	
+	$out ='<div class="card-box-33 col-md-3">
+		<img src="'.$url.'" class="card-grid-icon">
+		<h5 class="card-grid-title" >'.$title.'</h5>
+		<p class="card-grid-decription" >'.$content.'</p>
+		<h4 class="card-grid-link" title="'.$button_text.'">
+			<a class="uw-btn btn-sm" href="'.$button_link.'" target="_blank" rel="noopener noreferrer">'.$button_text.'</a>
+		</h4>
+		</div>
+		';
+	return $out;
+}
+add_shortcode( 'card', 'isc_card' );
+
 ?>
